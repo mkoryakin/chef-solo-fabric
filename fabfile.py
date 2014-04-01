53edf49f0fe3baf57a04e9db7cbe924fb5913aca
MKoryakin <maksim@mkoryakin.com>
2014-04-01 04:39:25 -0400

    add chef files

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,13 @@
 from fabric.api import run, env, sudo
+from fabric.contrib.project import rsync_project
 
 env.hosts = [
 'ec2-54-72-67-27.eu-west-1.compute.amazonaws.com'
 ]
 
 env.user = 'ubuntu'
-
 env.key_filename = '/root/.ssh/aws.pem'
+env.path = '/tmp/'
 
 def hello(name="world"):
     print("Hello %s!" % name)
@@ -35,4 +36,4 @@ def ruby_install():
 
 def chef_install():
      sudo("gem update --no-rdoc --no-ri")
-     sudo("gem install ohai chef --no-rdoc --no-ri")
+     sudo("gem install ohai chef --no-rdoc --no-ri"
